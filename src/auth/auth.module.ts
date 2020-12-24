b39bef920d4d38d2fba5642b27ffd8aa42576f47
tomisicm <tomisicm@protonmail.com>
2020-12-24 22:00:19 +0100

    feature config files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,9 @@ import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { UserRepository } from './user.repository'
 import { JwtStrategy } from './jwt.strategy'
+import * as config from 'config'
+
+const authConfig = config.get('jwt')
 
 @Module({
   imports: [
@@ -13,9 +16,9 @@ import { JwtStrategy } from './jwt.strategy'
       defaultStrategy: 'jwt'
     }),
     JwtModule.register({
-      secret: 'topsecret',
+      secret: process.env.JWT_SECRET || authConfig.secret,
       signOptions: {
-        expiresIn: 3600
+        expiresIn: authConfig.expiresIn
       }
     }),
     TypeOrmModule.forFeature([
