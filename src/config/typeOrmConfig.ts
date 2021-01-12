e75b194396fe4b266b55169eda54b54e7f77c347
tomisicm <tomisicm@protonmail.com>
2021-01-12 16:02:33 +0100

    qwe

--- src/config/typeOrmConfig.ts
+++ src/config/typeOrmConfig.ts
@@ -3,15 +3,19 @@ import * as config from 'config'
 
 const dbConfig = config.get('db')
 
-// console.error(dbConfig)
+console.error(dbConfig)
+console.error('--------------------------------------')
+console.error(process.env)
+console.error('--------------------------------------')
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: dbConfig.type,
     host: dbConfig.host,
-    port: dbConfig.port,
-    username: dbConfig.username,
-    password: dbConfig.password,
-    database: dbConfig.dababase,
+    // port: dbConfig.port,
+    // username: dbConfig.username,
+    // password: dbConfig.password,
+    // database: dbConfig.dababase,
+    url: dbConfig.url,
     entities: [__dirname + '/../**/*.entity.js'],
     synchronize: dbConfig.synchronize
 }
