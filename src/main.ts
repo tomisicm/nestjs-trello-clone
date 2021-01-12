2b312ec37008bb8b146786b39fa2e9fb9bce4c0c
tomisicm <tomisicm@protonmail.com>
2021-01-12 20:03:39 +0100

    port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as config from 'config'
 async function bootstrap() {
     const serverConfig = config.get('server')
     const logger = new Logger('bootstrap')
-    const port = serverConfig.port
+    const port = process.env.PORT || serverConfig.port
 
     const app = await NestFactory.create(AppModule)
     await app.listen(port)
